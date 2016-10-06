74119db1903044c550ab74990f406e38a0bb3aa7
David Gibb <dgibb802@gmail.com>
2016-10-06 14:51:53 -0400

    Update Counting Bits.js

--- Medium/Counting-Bits/Counting Bits.js
+++ Medium/Counting-Bits/Counting Bits.js
@@ -1,7 +1,16 @@
-/**
- * @param {number} num
- * @return {number[]}
+/**338. Counting Bits
+https://leetcode.com/problems/counting-bits/
+Difficulty: Medium
+
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+@param {number} num
+@return {number[]}
  */
+
 var countBits = function(num) {
     var number=[];                      
     for (var i = 0; i <= num; i++) {    
