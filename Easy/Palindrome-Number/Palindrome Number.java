635b8d9af407d1229846c3ebc3be87a5373c7ca1
David Gibb <dgibb802@gmail.com>
2016-10-06 15:07:27 -0400

    Update Palindrome Number.java

--- Easy/Palindrome-Number/Palindrome Number.java
+++ Easy/Palindrome-Number/Palindrome Number.java
@@ -1,11 +1,16 @@
+/*
+9. Palindrome Number
+https://leetcode.com/problems/palindrome-number/
+Difficulty: Easy
+
+Determine whether an integer is a palindrome. Do this without extra space.
+*/
+
 public class Solution {
     public boolean isPalindrome(int x) {
         boolean palindrome=true;
-        System.out.println(x);
         char [] numbers = String.valueOf(x).toCharArray();
-        System.out.println(numbers.length);
         for (int i=0; i<numbers.length-1; i++){
-               System.out.println(i);
             if (numbers[i]!=numbers[numbers.length-(i+1)]){
                 palindrome=false;
             }
