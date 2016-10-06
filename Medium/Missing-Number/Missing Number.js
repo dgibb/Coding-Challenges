370416bb2b6cce1b83ee8931cb030eb22192f812
David Gibb <dgibb802@gmail.com>
2016-10-06 14:53:35 -0400

    Update Missing Number.js

--- Medium/Missing-Number/Missing Number.js
+++ Medium/Missing-Number/Missing Number.js
@@ -1,7 +1,19 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+/*268. Missing Number
+https://leetcode.com/problems/missing-number/
+Difficulty: Medium
+
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity? 
+
+@param {number[]} nums
+@return {number}
+*/
+
 var missingNumber = function(nums) {
     var boolArray=[];
     for (var i=0; i<nums.length+1;i++){
